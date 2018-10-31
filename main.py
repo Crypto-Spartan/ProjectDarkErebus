27320512bbc39f3d3c6975e1e2fab46ae3f401ba
Tyler Belfield <tylerbelfield@gmail.com>
2018-10-31 15:44:07 +0000

    Replace main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-#import build_table
+
 #import pandas as pd
+#pd.set_option('max_columns', 50)
 #import build_table
-
 #import get_winners as winners
+#pd.set_option('max_columns', 50)
 
-#winners.ask_user_for_winners('5')
+#winners.ask_user_for_winners('7')
 
 import machine_learning
+
+#week5 = pd.read_csv('final_matchups_week5.csv', index_col=0)
+#week6 = pd.read_csv('final_matchups_week6.csv', index_col=0)
+#week7 = pd.read_csv('final_matchups_week7.csv', index_col=0)
+#week8 = pd.read_csv('final_matchups_week8.csv', index_col=0)
+#print(week8)
+
+#matchup_data = pd.concat([week5, week6])
+#matchup_data = matchup_data.reset_index()
+
+#print(matchup_data[['Away Team','AT','Home Team','HmT_OffRk(yds)']].to_string())
+#print(week7[['Away Team','AT','Home Team','HmT_DefRk(yds)']].to_string())
+
+
+#for index,row in week7.iterrows():
+#  for index,row in row.iteritems():
+#    if type(row) == float or type(row) == int:
+#      pass
+#    else:
+#      print(index,type(row))
+
+
+
+
+
 
 
