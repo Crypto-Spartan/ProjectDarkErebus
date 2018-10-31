50191985a2c64af85055bf0f10f84e6e959a5bd9
Tyler Belfield <tylerbelfield@gmail.com>
2018-10-31 14:59:30 +0000

    Replace main.py

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-#Get NFL Team Stats
+#import build_table
+#import pandas as pd
+#import build_table
 
-import csv
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+#import get_winners as winners
 
-url = "http://www.espn.com/nfl/statistics/team/_/stat/total"
-req = requests.get(url)
-soup = BeautifulSoup(req.content, 'html.parser')
+#winners.ask_user_for_winners('5')
 
-stats_table = soup.find('table', class_='tablehead')
-
-list_of_rows = []
-for row in stats_table.findAll('tr'):
-    list_of_cells = []
-    for cell in row.findAll(["th","td"]):
-        text = cell.text
-        list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
+import machine_learning
 
 
-for item in list_of_rows:
-    #print(' '.join(item))
-    print(item)
-
-with open('nfl_stats.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerows(list_of_rows)
